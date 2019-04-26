2bef36bbb8dc6267b4818372e388802e5818cddd
Saeed Sheikh <saeedshaikh166@gmail.com>
2019-04-25 22:28:17 -0400

    Initial incorporation of three js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { loadVideo, estimatePose } from './estimate.js';
+import { animate } from './environment';
 
 async function performEstimation() {
   const imageElement = await loadVideo();
   const poseToReturn = await estimatePose(imageElement);
+  console.log(poseToReturn);
   return poseToReturn;
 }
 
 performEstimation();
+animate();
