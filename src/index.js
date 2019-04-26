d340ff411604ec73717965351f805b0d5df6c323
Saeed Sheikh <saeedshaikh166@gmail.com>
2019-04-25 21:40:47 -0400

    Built with webpack, app being served by webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { loadVideo, estimatePose } from './estimate.js';
 async function performEstimation() {
   const imageElement = await loadVideo();
   const poseToReturn = await estimatePose(imageElement);
-  console.log(poseToReturn);
   return poseToReturn;
 }
 
