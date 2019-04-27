c301d0d0615ab088ebb2ae4ccfcfc4b5a5edd2cc
Saeed Sheikh <saeedshaikh166@gmail.com>
2019-04-27 11:13:47 -0400

    Committing work for incorporating model

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 #For Fullstack Senior phase stackathon
 #Saeed Sheikh Apr - 2019
 
+#model in app is from mixamo.com https://www.mixamo.com/
--- src/animate.js
+++ src/animate.js
@@ -5,19 +5,18 @@ import GLTFLoader from 'three-gltf-loader';
 
 const start = new Date();
 
-
-var scene, renderer, camera, stats;
-var model, skeleton, mixer, clock;
-var crossFadeControls = [];
-var idleAction, walkAction, runAction;
-var idleWeight, walkWeight, runWeight;
-var actions, settings;
-var singleStepMode = false;
-var sizeOfNextStep = 0;
+let scene, renderer, camera, stats;
+let model, skeleton, mixer, clock;
+let crossFadeControls = [];
+let idleAction, walkAction, runAction;
+let idleWeight, walkWeight, runWeight;
+let actions, settings;
+let singleStepMode = false;
+let sizeOfNextStep = 0;
 init();
 
 function init() {
-  var container = document.getElementById('animation-container');
+  const container = document.getElementById('animation-container');
   camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
@@ -30,10 +29,10 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xa0a0a0);
   scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
-  var hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
   hemiLight.position.set(0, 20, 0);
   scene.add(hemiLight);
-  var dirLight = new THREE.DirectionalLight(0xffffff);
+  const dirLight = new THREE.DirectionalLight(0xffffff);
   dirLight.position.set(-3, 10, -10);
   dirLight.castShadow = true;
   dirLight.shadow.camera.top = 2;
@@ -44,14 +43,14 @@ function init() {
   dirLight.shadow.camera.far = 40;
   scene.add(dirLight);
 
-  var mesh = new THREE.Mesh(
+  const mesh = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(100, 100),
     new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
   );
   mesh.rotation.x = -Math.PI / 2;
   mesh.receiveShadow = true;
   scene.add(mesh);
-  var loader = new GLTFLoader();
+  const loader = new GLTFLoader();
   loader.load('Soldier.glb', function(gltf) {
     model = gltf.scene;
     scene.add(model);
@@ -63,13 +62,13 @@ function init() {
     skeleton.visible = false;
     scene.add(skeleton);
 
-    var animations = gltf.animations;
+    const animations = gltf.animations;
     mixer = new THREE.AnimationMixer(model);
     idleAction = mixer.clipAction(animations[0]);
     walkAction = mixer.clipAction(animations[3]);
     runAction = mixer.clipAction(animations[1]);
     actions = [idleAction, walkAction, runAction];
-    animate();
+    animationLoop();
   });
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
